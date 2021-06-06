7a713acea86a6da04fd98eceb1761a877203bb9d
Sidali <SidaliNadji@users.noreply.github.com>
2021-06-06 14:06:34 +0100

    Change images base url

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -19,6 +19,6 @@ class Post extends Model
         if (Str::contains($value, "http")) {
             return $value;
         }
-        return 'http://www.2er.net/Content/Upload/large/' . $value;
+        return 'https://arabi21.com/Content/Upload/large/' . $value;
     }
 }
