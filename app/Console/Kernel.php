e5486680561840f34519e6a7b9120e6c7f64dfdd
Sidali <SidaliNadji@users.noreply.github.com>
2021-06-13 20:43:03 +0100

    Add horizon

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('youtube:fetch')->hourly();
+        $schedule->command('youtube:process')->hourly();
     }
 
     /**
